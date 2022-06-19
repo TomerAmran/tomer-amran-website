b78eceb5211327727c832eeb9aa9036669f1646f
tomera <tomera@wix.com>
2022-06-19 21:22:52 +0300

    love niva

--- app/app.jsx
+++ app/app.jsx
@@ -1,11 +1,19 @@
 const React = require('react')
-
 const App = () => {
     const [count, setCount] = React.useState(0)
+    const hearts = Array(count).fill(1).map((_)=>{
+
+        return (<div>&#10084;&#65039;</div>)
+    }
+    )
+    console.log(hearts)
     return (
         <div>   
-            <h1>{count}</h1>
+            <div>
+                Click the button and love Niva.
+                </div>
             <button onClick={()=> setCount(count+1)}>click me</button>
+            {hearts}
         </div>
     )
 }
