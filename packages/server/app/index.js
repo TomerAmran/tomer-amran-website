4ad137547a43ac2442e5fb32df43d1fb5e06fdc7
tomera <tomera@wix.com>
2022-06-19 13:15:43 +0300

    simple app

--- packages/server/app/index.js
+++
@@ -1,7 +0,0 @@
-const root = document.getElementById("root");
-const newButton = document.createElement("button");
-newButton.innerHTML = "click me";
-newButton.addEventListener("click", () => console.log("button clicked"));
-setTimeout(() => {
-  root.appendChild(newButton);
-}, 1000);
