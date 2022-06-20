a3318378695613147db9f68915dbd26893d99843
tomera <tomera@wix.com>
2022-06-20 17:54:18 +0300

    posts

--- app/blog.jsx
+++ app/blog.jsx
@@ -1,42 +1,57 @@
-const axios = require('axios')
-const { useEffect } = require('react')
-const React = require('react')
-const SERVER_URL = 'http://tomeramran.com'
+const axios = require("axios");
+const { useEffect } = require("react");
+const React = require("react");
+// const SERVER_URL = "http://tomeramran.com";
+const SERVER_URL = "http://localhost:3000";
 
-const Blog = ()=>{
-    const [posts, setPosts] = React.useState([])
-    const fetchPosts = ()=>{
-        return axios.get(`${SERVER_URL}/post`).then(({data})=>{
-            setPosts(data)
-        })
-    }
+const Blog = () => {
+  const [posts, setPosts] = React.useState([]);
+  const fetchPosts = () => {
+    return axios.get(`${SERVER_URL}/post`).then(({ data }) => {
+      setPosts(data);
+    });
+  };
 
-    const addPost =()=>{
-        return axios.post(`${SERVER_URL}/post`,{
-            title: 'New Post' + Math.random(),
-            body: 'This is a new post'+ Math.random(),
-            author : 'Niva'+ Math.random()
-        })
-    }
+  const addPost = (event) => {
+    event.preventDefault();
+    const post = {
+      name: document.getElementById("name").value,
+      title: document.getElementById("title").value,
+      body: document.getElementById("body").value,
+    };
+    return axios.post(`${SERVER_URL}/post`, post);
+  };
 
-    useEffect(fetchPosts,[])
-    return(
+  useEffect(fetchPosts, []);
+  return (
+    <div>
+      <h1>Blog</h1>
+      <button onClick={fetchPosts}>fetch posts</button>
+      <form onSubmit={addPost}>
+        <label>
+          Name:
+          <input id="name" type="text" name="name" />
+        </label>
+        <label>
+          Title:
+          <input id="title" type="text" name="title" />
+        </label>
+        <label>
+          Body:
+          <input id="body" type="text" name="body" />
+        </label>
+        <input type="submit" value="Add Post" />
+      </form>
+
+      {posts.map((post) => (
         <div>
-            <h1>Blog</h1>
-            <button onClick={()=> addPost()}>add post</button>
-            <button onClick={fetchPosts}>fetch posts</button>
-
-            {posts.map((post)=>(
-                    <div>
-                        <h2>{post.title}</h2>
-                        <h5>{post.author}</h5> 
-                        <p>{post.body}</p>
-                    </div>
-                ))
-            }
+          <h2>{post.title}</h2>
+          <h5>{post.author}</h5>
+          <p>{post.body}</p>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-
-module.exports = Blog
+module.exports = Blog;
