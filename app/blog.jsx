7bdd8d02fb4e3d62a5dadd38227425b1eba9ce33
tomera <tomera@wix.com>
2022-06-20 17:38:11 +0300

    remove :3000: '

--- app/blog.jsx
+++ app/blog.jsx
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const { useEffect } = require('react')
 const React = require('react')
-const SERVER_URL = 'http://tomeramran.com:3000'
+const SERVER_URL = 'http://tomeramran.com'
 
 const Blog = ()=>{
     const [posts, setPosts] = React.useState([])
